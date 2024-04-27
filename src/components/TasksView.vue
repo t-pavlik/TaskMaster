04d7e746d12ab63efad2bbc6c195dd7fe57eaa46
Tomas Pavlik <tomas.pavlik2237@seznam.cz>
2024-04-27 13:18:55 +0200

    Zmena barev karet

--- src/components/TasksView.vue
+++ src/components/TasksView.vue
@@ -3,23 +3,38 @@
       @submit.prevent="addTask"
   >
     <div class="task">
-      <div class="field is-grouped">
-        <div class="control is-expanded">
-          <input
-              v-model="newContent"
-              class="input"
-              type="text"
-              placeholder="Add task"
-          >
+      <div class="box">
+        <div class="field">
+          <label class="label">Task</label>
+          <div class="control">
+            <input
+                v-model="newTaskName"
+                class="input"
+                type="text"
+                maxlength="25"
+                placeholder="Task name"
+            >
+          </div>
         </div>
-        <div class="control">
-          <button
-              :disabled="!newContent"
-              class="button is-info"
-          >
-            Add
-          </button>
+
+        <div class="field">
+          <label class="label">Info</label>
+          <div class="control">
+              <textarea
+                  v-model="newInfo"
+                  class="textarea"
+                  type="text"
+                  placeholder="Information about task"
+              ></textarea>
+          </div>
         </div>
+
+        <button
+            :disabled="!newTaskName || !newInfo"
+            class="button is-info"
+        >
+          Add
+        </button>
       </div>
     </div>
   </form>
@@ -30,23 +45,28 @@
           v-for="task in tasks"
           class="card"
       >
-        <header class="card-header">
+        <header class="card-header" :style="{backgroundColor: task.color}">
           <p class="card-header-title">{{ task.name }}</p>
         </header>
         <div class="card-content">
           <div class="content">
-            {{ task.id }}
+            {{ task.content }}
           </div>
         </div>
-        <footer class="card-footer">
-          <button
-              class="card-footer-item">Done
-          </button>
+        <div class="card-footer">
+          <div class="select is-rounded">
+            <select @change="onChange(task.id, $event)">
+              <option value="1">Open</option>
+              <option value="2">In progress</option>
+              <option value="3">Done</option>
+              <option value="4">Won't do</option>
+            </select>
+          </div>
           <button
               @click="deleteTask(task.id)"
               class="card-footer-item">Delete
           </button>
-        </footer>
+        </div>
       </div>
     </div>
   </div>
@@ -59,20 +79,35 @@ import {v4 as uuidv4} from "uuid"
 
 const tasks = ref([])
 
-const newContent = ref("");
+const newTaskName = ref("");
+const newInfo = ref("");
+
+const colorMap = {
+  1: "",
+  2: "blue",
+  3: "green",
+  4: "red"
+}
 
 const addTask = () => {
   const newTask = {
     id: uuidv4(),
-    name: newContent.value,
-    content: "Test"
+    name: newTaskName.value,
+    content: newInfo.value,
+    color: colorMap[1]
   }
   tasks.value.push(newTask)
-  newContent.value = ""
+  newTaskName.value = ""
+  newInfo.value = ""
+  console.log(newTask)
 }
 
 const deleteTask = id => {
   tasks.value = tasks.value.filter(task => task.id !== id)
+}
+
+const onChange = (id, event) => {
+  tasks.value.find(task => task.id === id).color = colorMap[event.target.value];
 }
 </script>
 
@@ -81,5 +116,9 @@ const deleteTask = id => {
   max-width: 600px;
   padding: 20px;
   margin: 0 auto;
+}
+
+.card {
+  background-color: green;
 }
 </style>
